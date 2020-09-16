6394359b1d30c190567b51edc3f6f7ea3143cf96
Jessica Waltz <jessabelleanna@gmail.com>
2020-09-16 18:06:42 -0400

    for nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link, HashRouter } from "react-router-dom";
 import './App.css';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       Hello
     </div>
   );
