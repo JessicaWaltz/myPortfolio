a9602740d06f976eedc02e2f8cc662befee8a4ba
Jessica Waltz <jessicawaltz93@gmail.com>
2020-10-29 13:07:17 -0400

    preparing to add project 1

---
+++ src/components/body/Proj1.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+//import "./AboutMe.css";
+//test
+class Proj1 extends Component{
+    render(){
+        return(
+            <div className="Proj1-conatiner" id="Proj1">
+                <div className="name">
+                    Pokemon Battle Strategy
+                </div>
+                <div>
+                    The Pokemon Battle Strategy Project uses the (link)pokeApi(link). 
+                    With this data the code will calculate the best type advantage to take in order to combat 
+                    the selected Pokemon. It also calculates the types the user should avoid.
+                </div>
+                <div>Add visual and have link to page it is on</div>
+            </div>
+        )
+    }
+}
+export default Proj1;
