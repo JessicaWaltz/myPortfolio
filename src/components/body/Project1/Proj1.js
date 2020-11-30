ab71465e5c9994b83dba203b8dc7bc1d9866a8e0
Jessica Waltz <jessicawaltz93@gmail.com>
2020-11-30 09:49:50 -0500

    added info for proj1 and starting info for proj2

--- src/components/body/Project1/Proj1.js
+++ src/components/body/Project1/Proj1.js
@@ -12,13 +12,19 @@ class Proj1 extends Component{
                     World Of Dogs
                 </div>
                 <div className="proj1-desc-container project-description">
-                    World of dogs is an application that holds information about many dogs such as 
+                    <div className="proj1-desc-title" id="about"> About:</div>
+                    <div>World of dogs is an application that holds information about many dogs such as 
                     their name, breed, size, owner, and a description.This application has multiple 
                     routes with the ability to display all the dogs, sort the dogs by their information,
                     add new dogs, edit/delete old dogs, and return a random dog from the list. 
-                    This application uses Redux to store the state of all the dogs in the list, in the future 
+                    </div>
+                    <div className="proj1-desc-title" id="what-is-used">What is used:</div>
+                    <div>This application uses Redux to store the state of all the dogs in the list, in the future 
                     the dogs will be added to a database so the dog list can be permanently updated and doesnt revert
-                    back to its initial state.
+                    back to its initial state.</div>
+                    <div className="proj1-desc-title" id="what-is-planned">What is Planned:</div>
+                    <div>In the future the dogs will be added to a database so the dog list can be permanently updated 
+                    and doesnt revert back to its initial state when the apllication is refreshed.</div>
                 </div>
                 <div className="proj1-visual-conatainer animate__animated animate__fadeInUp">
                     <div className="proj1-visual-pics">
