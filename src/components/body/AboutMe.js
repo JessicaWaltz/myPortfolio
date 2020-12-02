22e17b028d22d3aad2222c2e46ab3c1c04e947e7
Jessica Waltz <jessicawaltz93@gmail.com>
2020-12-02 15:32:15 -0500

    fixed about me info

--- src/components/body/AboutMe.js
+++ src/components/body/AboutMe.js
@@ -9,11 +9,15 @@ class AboutMe extends Component{
                     <div className="AboutMe-title">
                         About me
                     </div>
-                    <div className="AboutMe-info">
+                    <div className="AboutMe-intro">
                     Hello and welcome to my portfolio! My name is Jessica Waltz and I am a full stack developer 
                     from Washington State University. I enjoy front end work the most. I use React in my projects 
                     with Spring boot for backend. Check out my github for my project progress, I will be posting 
                     some of them on here in the near future. 
+                    </div>
+                    <div className="AboutMe-info">
+                        re-word later: intrest in computers at young age, started making flash games in as3, started
+                         intrests in making websites because of neopets. Love dogs 
                     </div>
                 </div>
                 <div className="AboutMe-deg">
@@ -39,11 +43,12 @@ class AboutMe extends Component{
                             editing requirements for said UI through the use of jsp, java, and javascript.</li> 
                             <li>Knowledge of database manipulation and testing with the use of DataStudio, MongoDB, and SoapUI</li> 
                             <li>Adding Queues and their requirements for queueing and routing</li>
+                            <li>Worked for this company through Revature</li>
                         </ul> 
                     </div>
                     <div>
                         <h2>Revature, Tampa FL - Full Stack Developer</h2>
-                        July 2019 - November 2019
+                        July 2019 - Current
                         <ul>
                             <li>Experience in making Single Page Applications using tools like React and Bootstrap</li>
                             <li>Created back-ends for said Single Page Applications in both Typescript/JavaScript, 
